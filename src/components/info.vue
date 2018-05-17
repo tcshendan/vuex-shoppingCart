b41d6213d80b00c3188a83cfcb0f3e844992138d
shendan <664542051@qq.com>
2018-05-17 15:08:35 +0800

    init

---
+++ src/components/info.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="info">
+    <h4>结算信息</h4>
+    <div class="item-wrapper">
+      <div class='item'>总数：
+        <strong>{{totalNum}}</strong>
+      </div>
+      <div class='item'>总价：
+        <strong>{{totalPrice}}</strong>
+      </div>
+      <div class="item btn btn-danger">清空购物车</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'info',
+  data () {
+    return {
+      totalNum: 13,
+      totalPrice: 342
+    }
+  }
+}
+</script>
+<style scoped>
+  .item-wrapper {
+    display: flex;
+    background-color: #dfdfdf;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .item {
+    flex: 1;
+  }
+</style>
---
+++ src/components/product.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="product">
+    <h4>商品信息</h4>
+    <table class="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>名称</th>
+          <th>价格</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(shop, index) in shoplist" :key="index">
+          <td>{{shop.id}}</td>
+          <td>{{shop.name}}</td>
+          <td>{{shop.price}}</td>
+          <td>
+            <div class="btn btn-info">购物车</div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'product',
+  data () {
+    return {
+      shoplist: [
+        {
+          id: 11,
+          name: '鱼香肉丝',
+          price: 12
+        },
+        {
+          id: 22,
+          name: '宫保鸡丁',
+          price: 14
+        },
+        {
+          id: 34,
+          name: '土豆丝',
+          price: 10
+        },
+        {
+          id: 47,
+          name: '米饭',
+          price: 2
+        }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+  .table th {
+    text-align: center;
+  }
+</style>
