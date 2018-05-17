4a5a9cb71f29841297d91556cbc01a7c5a81a35d
shendan <664542051@qq.com>
2018-05-17 14:19:08 +0800

    引入bootstrap

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'assets': path.resolve(__dirname, '../src/assets')
     }
   },
   module: {
